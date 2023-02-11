8d66ec6675867839b126ada481a46bb3d0422c88
mahmutunder <mahmutunder@gmail.com>
2023-02-10 20:48:36 -0700

    fixed some bugs

--- src/countdownTimerGUI360_60s/CountdownTimerGUI360.java
+++ src/countdownTimerGUI360_60s/CountdownTimerGUI360.java
@@ -2,12 +2,10 @@ package countdownTimerGUI360_60s;
 
 import java.awt.Color;
 import java.awt.Dimension;
-
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
-
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.text.DecimalFormat;
